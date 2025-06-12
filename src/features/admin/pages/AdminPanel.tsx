e0959d357fe81d9e0a8e43e237c93c967a74cef0
KoboldM <mikoi.lukban@gmail.com>
2025-06-12 10:37:59 +0800

    fix on admin panel messages height issue and padding issue for all content

--- src/features/admin/pages/AdminPanel.tsx
+++ src/features/admin/pages/AdminPanel.tsx
@@ -180,12 +180,18 @@ export default function AdminPanel(): JSX.Element {
                 <div className="min-h-screen bg-slate-900 p-2 md:p-8">
                     <div className="max-w-7xl mx-auto">
                         <AdminHeader handleSignOut={handleSignOut}/>      
-                        <div
+                        {/* <div
                             className={`bg-slate-800/90 backdrop-blur-sm rounded-xl ${
                                 activeTab !== "messages" ? "p-6 md:p-8" : "p-0"
                             } shadow-xl border border-slate-700/50 relative overflow-hidden ${
                                 activeTab == "users" ? "min-h-[90vh]" : ""
                             } ${activeTab == "messages" ? "h-[calc(100vh-7rem)]" : ""}`}
+                        > */}
+                        <div
+                            className={`bg-slate-800/90 backdrop-blur-sm rounded-xl 
+                            shadow-xl border border-slate-700/50 relative overflow-hidden ${
+                                activeTab == "users" ? "min-h-[90vh]" : ""
+                            } `}
                         >
                             {/* Background gradient effects */}
                             <div
@@ -202,9 +208,8 @@ export default function AdminPanel(): JSX.Element {
                                 }`}
                             >
                                 <div
-                                    className={activeTab == "messages" ? "p-4 bg-slate-800/80" : ""}
+                                    className={`p-4 ${activeTab == "messages" ? "bg-slate-800/80" : ""}`}
                                 >
-                                    
                                     <AdminTabs
                                         navigate={navigate}
                                         activeTab={activeTab}
